294129ff3be34a7fca0bcefafdf456eb400835b7
ysnsk <ysnsktmi@gmail.com>
2015-02-19 01:00:01 +0900

    function and gulplive

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var csscomb = require('gulp-csscomb');
 var spritesmith = require('gulp.spritesmith');
 var styledocco = require('gulp-styledocco');
+var connect = require('gulp-connect');
 
 gulp.task('styledocco', function () {
   gulp.src('dist/*.css')
@@ -30,8 +31,34 @@ gulp.task('sass', function () {
   .pipe(sass())
   .pipe(autoprefixer("last 2 version", "ie 8", "ie 7"))
   .pipe(csscomb())
-  .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest('./dist'))
+  .pipe(connect.reload());
 });
+
+gulp.task('copy', function(){
+  gulp.src('*.html')
+    .pipe(gulp.dest('./livecoding'))
+    .pipe(connect.reload());
+});
+gulp.task('js', function(){
+  gulp.src('dist/*.js')
+    .pipe(gulp.dest('./livecoding'))
+    .pipe(connect.reload());
+});
+
+gulp.task('serve', function(){
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('watch', ['sass', 'copy', 'js'], function(){
+  gulp.watch('src/*.scss', ['sass']);
+  gulp.watch('dist/*.js', ['js']);
+  gulp.watch('*.html', ['copy']);
+});
+
+gulp.task('live',['watch','serve'])
 
 gulp.task('sprite', function () {
     var spriteData = gulp.src('src/sprite/*')
